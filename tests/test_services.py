0e0525bf0336787799d57642f8c6880c75f4c344
jbernal <j.bernal@upm.es>
2021-01-14 22:01:24 +0100

    added coverage

--- tests/test_services.py
+++ tests/test_services.py
@@ -11,10 +11,9 @@ class TestService(unittest.TestCase):
         disconnect()
         connect('mongoenginetest', host='mongomock://localhost')
 
+    def test_created_read(self):
+        one = self.service.create(123456, ModificationComplaint(barcode='123456', description='123456'))
+        self.assertIsNotNone(self.service.read(123456,one.id))
+
     def tearDown(self) -> None:
         disconnect()
-
-    def test_created_read(self):
-        one = self.service.create(ModificationComplaint(mobile=123456, barcode='123456', description='123456'))
-        self.assertIsNotNone(self.service.read(one.id))
-

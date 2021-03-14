d2f87975e262db182e22ab948263eac7e31fdd56
MRxRafi <rafadriveclase@gmail.com>
2021-03-14 18:23:06 +0100

    Completed review_data methods + tests. Deleted mocks #102

--- src/data/review_data.py
+++ src/data/review_data.py
@@ -1,4 +1,7 @@
-from mongoengine import Document, IntField, StringField, FloatField
+from fastapi import HTTPException, status
+from mongoengine import Document, IntField, StringField, FloatField, DoesNotExist
+
+from src.models.review import Review, DBReview
 
 
 class ReviewDocument(Document):
@@ -13,3 +16,42 @@ class ReviewDocument(Document):
     def dict(self):
         return {'id': str(self.id), 'mobile': self.mobile, 'barcode': self.barcode, 'score': str(self.score),
                 'opinion': self.opinion}
+
+
+def create(db_review) -> Review:
+    review_document = ReviewDocument(**db_review.dict())
+    review_document.save()
+    return Review(**review_document.dict())
+
+
+def __read_assured(identifier):
+    try:
+        return ReviewDocument.objects(id=identifier).get()
+    except DoesNotExist:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found Review: " + identifier)
+
+
+def read(identifier) -> DBReview:
+    review_document = __read_assured(identifier)
+    return DBReview(**review_document.dict())
+
+
+def update(review) -> Review:
+    review_document = __read_assured(review.id)
+    review_document.update(**review.dict())
+    updated_review = read(review_document.id)
+    return Review(**updated_review.dict())
+
+
+def find_by_mobile(mobile) -> [Review]:
+    reviews = []
+    for item in ReviewDocument.objects(mobile=mobile):
+        reviews.append(Review(**item.dict()))
+    return reviews
+
+
+def find_all() -> [DBReview]:
+    reviews = []
+    for item in ReviewDocument.objects():
+        reviews.append(DBReview(**item.dict()))
+    return reviews
